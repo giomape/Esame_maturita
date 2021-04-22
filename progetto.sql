28e5027ef329f8c2934933951eb7312c072cd5f5
giomape <78256794+giomape@users.noreply.github.com>
2021-04-22 13:29:50 +0200

    Aggiornamento del database per registrazione

--- progetto.sql
+++ progetto.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 18, 2021 alle 18:59
--- Versione del server: 10.4.18-MariaDB
--- Versione PHP: 8.0.3
+-- Creato il: Apr 22, 2021 alle 13:27
+-- Versione del server: 10.1.38-MariaDB
+-- Versione PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -20,6 +21,40 @@ SET time_zone = "+00:00";
 --
 -- Database: `progetto`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `calciatori`
+--
+
+CREATE TABLE `calciatori` (
+  `id_calciatore` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cognome` varchar(255) NOT NULL,
+  `data_nascita` date NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `nome_residenza` varchar(255) NOT NULL,
+  `latitudine` decimal(10,0) NOT NULL,
+  `longitudine` decimal(10,0) NOT NULL,
+  `piede` varchar(40) NOT NULL,
+  `biografia` varchar(500) NOT NULL,
+  `max_serie` varchar(50) NOT NULL,
+  `current_serie` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `calciatori_ruoli`
+--
+
+CREATE TABLE `calciatori_ruoli` (
+  `id_calciatore` int(11) NOT NULL,
+  `id_ruolo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -52,9 +87,50 @@ INSERT INTO `ruoli` (`id_ruolo`, `nome`, `gruppo`) VALUES
 (12, 'Attaccante esterno sinistro', 'Attaccante'),
 (13, 'Attaccante esterno destro', 'Attaccante');
 
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `societa`
+--
+
+CREATE TABLE `societa` (
+  `id_societa` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `nome_residenza` varchar(255) NOT NULL,
+  `latitudine` decimal(10,0) NOT NULL,
+  `longitudine` decimal(10,0) NOT NULL,
+  `current_serie` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `societa_ruoli`
+--
+
+CREATE TABLE `societa_ruoli` (
+  `id_societa` int(11) NOT NULL,
+  `id_ruolo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indici per le tabelle scaricate
 --
+
+--
+-- Indici per le tabelle `calciatori`
+--
+ALTER TABLE `calciatori`
+  ADD PRIMARY KEY (`id_calciatore`);
+
+--
+-- Indici per le tabelle `calciatori_ruoli`
+--
+ALTER TABLE `calciatori_ruoli`
+  ADD KEY `calciatore` (`id_calciatore`),
+  ADD KEY `ruolo` (`id_ruolo`);
 
 --
 -- Indici per le tabelle `ruoli`
@@ -63,14 +139,57 @@ ALTER TABLE `ruoli`
   ADD PRIMARY KEY (`id_ruolo`);
 
 --
+-- Indici per le tabelle `societa`
+--
+ALTER TABLE `societa`
+  ADD PRIMARY KEY (`id_societa`);
+
+--
+-- Indici per le tabelle `societa_ruoli`
+--
+ALTER TABLE `societa_ruoli`
+  ADD KEY `societa` (`id_societa`),
+  ADD KEY `ruoloo` (`id_ruolo`);
+
+--
 -- AUTO_INCREMENT per le tabelle scaricate
 --
+
+--
+-- AUTO_INCREMENT per la tabella `calciatori`
+--
+ALTER TABLE `calciatori`
+  MODIFY `id_calciatore` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `ruoli`
 --
 ALTER TABLE `ruoli`
   MODIFY `id_ruolo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT per la tabella `societa`
+--
+ALTER TABLE `societa`
+  MODIFY `id_societa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `calciatori_ruoli`
+--
+ALTER TABLE `calciatori_ruoli`
+  ADD CONSTRAINT `calciatore` FOREIGN KEY (`id_calciatore`) REFERENCES `calciatori` (`id_calciatore`),
+  ADD CONSTRAINT `ruolo` FOREIGN KEY (`id_ruolo`) REFERENCES `ruoli` (`id_ruolo`);
+
+--
+-- Limiti per la tabella `societa_ruoli`
+--
+ALTER TABLE `societa_ruoli`
+  ADD CONSTRAINT `ruoloo` FOREIGN KEY (`id_ruolo`) REFERENCES `ruoli` (`id_ruolo`),
+  ADD CONSTRAINT `societa` FOREIGN KEY (`id_societa`) REFERENCES `societa` (`id_societa`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
